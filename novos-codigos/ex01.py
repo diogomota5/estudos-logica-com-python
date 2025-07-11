129a77d1bdea9d7c59dc047c4e3f8f4c8217e276
Diogo Mota <diogo_mota1990@outlook.com>
2025-07-11 16:59:27 +0000

    Codigo criar variável e listas

--- novos-codigos/ex01.py
+++ novos-codigos/ex01.py
@@ -1,1 +1,6 @@
 # Neste algoritmo, crie uma variável que armazene uma string e uma lista que armazena várias strings.
+string = input("Digite uma string: ")
+lista = input("Digite várias strings separadas por vírgula: ").split(',')
+
+print("String:", string)
+print("Lista:", lista)
